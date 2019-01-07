8d4f4f9d266baafa5d8bbff188a696f557f14b94
remrama <mallett.remy@gmail.com>
2019-01-06 22:11:46 -0600

    msg python -> duino

--- src/print_sensor.py
+++ src/print_sensor.py
@@ -5,18 +5,34 @@ of reading serial input from duino into python.
 '''
 
 import serial
+import random
 
 duino = serial.Serial('/dev/cu.usbmodem52417201')
 
 while True:
     try:
         # grab data from arduino (teensy)
+        # ser_str = duino.readline(duino.inWaiting())
         ser_str = duino.readline()
         sep = ser_str.split('\r')
         # sometimes 2 vals get sent
         vals = [ float(x) for x in sep if x.isalnum() ]
         for v in vals:
             print v
+        #     # turn light on/off depending on value
+        #     if v > 500:
+        #         duino.write(bytes(1))
+        #     else:
+        #         duino.write(bytes(2))
+
+        # # sending msg to duino
+        # if random.choice([True,False]):
+        #     duino.write(bytes(1)) # str would work python2 as well
+        # else:
+        #     duino.write(bytes(2))
+        # # duino.write(b'test')
+        # # serial.time.sleep(2)
+
 
         # #### FOR TRYING TO USE BITS (not strings)
         # # grab data from arduino (teensy)
@@ -31,5 +47,5 @@ while True:
         #     print 'NO DATA DETECTED'
 
     except KeyboardInterrupt:
-        import sys
-        sys.exit()
+        duino.close()
+        serial.sys.exit()
