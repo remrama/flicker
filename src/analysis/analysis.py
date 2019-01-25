5ff6e0347d8dfd1ae28ef2936fcb60d0f8e2a887
remrama <mallett.remy@gmail.com>
2019-01-24 21:42:49 -0700

    spectogram

--- src/analysis/analysis.py
+++ src/analysis/analysis.py
@@ -1,30 +1,15 @@
 
-import sys, time
+import time
 import numpy as np
+from scipy import signal
 import matplotlib.pyplot as plt
 
 
-data_keys = {
-     1: 'left - EO',
-     2: 'left - EO',
-     3: 'left - EO',
-     4: 'left - EO',
-     5: 'left - EO',
-     6: 'down - EO',
-     7: 'right - EO',
-     8: 'up - EO',
-     9: 'left - EC',
-    10: 'down - EC',
-}
+data_num = 1
 
-try:
-    data_num = int(sys.argv[-1])
-except IndexError:
-    raise Warning('must enter data value atm')
-
-log_fname = './data{:02d}.log'.format(data_num)
-val_fname = './data{:02d}.csv'.format(data_num)
-plt_fname = './data{:02d}.png'.format(data_num)
+log_fname = '../../data/data{:02d}.log'.format(data_num)
+val_fname = '../../data/data{:02d}.csv'.format(data_num)
+plt_fname = '../../data/data{:02d}.png'.format(data_num)
 
 
 # import logfile as a list of entries
@@ -39,12 +24,23 @@ stamps, volts = np.loadtxt(val_fname,
                            unpack=True)
 
 
+# plot spectogram
+fs = 100 # sampling frequency
+f, t, Sxx = signal.spectrogram(volts,fs)
+
+plt.pcolormesh(t,f,Sxx)
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
+
+
+
 # inspecting the sampling rate
 n_samples = stamps.size
 n_secs = stamps[-1] - stamps[0]
 sample_rate = n_samples / n_secs # Hz
 print '{:d} samples in {:.02f} seconds ({:.02f} Hz)'.format(
     n_samples,n_secs,sample_rate)
+
 
 
 # grab timestamps of audio instructions
@@ -59,23 +55,23 @@ for s in soundtimes:
     unxtimes.append(unxtime)
 
 
-# plot timecourse of when audio was playing
-# and mark where audio started
-# NOTE: there is a small but sig lag between
-# when the audio "starts" and actually starts
-LAG = 20 # secs
-start_audio = unxtimes[0] - LAG
-end_audio = unxtimes[-1] + LAG
-audio_idx = np.logical_and(stamps>start_audio,
-                           stamps<end_audio)
+# # plot timecourse of when audio was playing
+# # and mark where audio started
+# # NOTE: there is a small but sig lag between
+# # when the audio "starts" and actually starts
+# LAG = 20 # secs
+# start_audio = unxtimes[0] - LAG
+# end_audio = unxtimes[-1] + LAG
+# audio_idx = np.logical_and(stamps>start_audio,
+#                            stamps<end_audio)
 
+# plt.plot(stamps[audio_idx],volts[audio_idx],c='k')
+# _ = [ plt.axvline(ut,c='r',ls='--') for ut in unxtimes ]
+# plt.title(data_keys[data_num])
 
-plt.plot(stamps[audio_idx],volts[audio_idx],c='k')
-_ = [ plt.axvline(ut,c='r',ls='--') for ut in unxtimes ]
-plt.title(data_keys[data_num])
+# plt.xlabel('Unix timestamp (s)')
+# plt.ylabel('Volts')
 
-plt.xlabel('Unix timestamp (s)')
-plt.ylabel('Volts')
+# plt.savefig(plt_fname)
+# plt.show()
 
-plt.savefig(plt_fname)
-plt.show()
